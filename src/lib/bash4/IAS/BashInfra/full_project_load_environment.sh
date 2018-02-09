4dfbdedb4eddf16265ff13f4798177dc992c440e
Martin VanWinkle <mvanwinkle@ias.edu>
2018-02-09 14:55:20 -0500

    checking it in.

--- src/lib/bash4/IAS/BashInfra/full_project_load_environment.sh
+++ src/lib/bash4/IAS/BashInfra/full_project_load_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ ! -f "$CONF_DIR"/environment.sh ]]; then
-	if [[ ! -z ${ENVIRONMENT_CONFIG_REQUIRED+x} ]]; then
+	if [[ "${ENVIRONMENT_CONFIG_REQUIRED}" == '1' ]]; then
 		>&2 echo "ERROR: $CONF_DIR/environment.sh doesn't exist."
 		>&2 echo "It should contain either:"
 		>&2 echo "ENVIRONMENT='PROD'"
