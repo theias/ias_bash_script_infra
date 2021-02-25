d2e4153e24f55fb0e9530b512adc4b9f8b193575
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-02-25 11:40:46 -0500

    progress

--- src/scratch/split_test.sh
+++ src/scratch/split_test.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+function get_project_whence
+{
+	echo "$script_dir"
+}
+
 function is_dir_in_src_dir
 {
 	# Returns true if a path ends with src/something
-	local dir=$( dirname "$1")
-	dir=$(basename $dir)
+	local dir_arg="$1"
+	# echo "Passed: $dir_arg"
+	local dir="${dir_arg:-`get_project_whence`}"
+	# echo "Examaning: $dir"
+
+	dir=$(dirname "$dir")
+	dir=$(basename "$dir")
 
 	if [[ "$dir" = "src" ]]
 	then
@@ -18,16 +28,8 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 real_script_dir=$(realpath "$script_dir")
 PATH="$real_script_dir/../lib/bash4:$PATH"
 
-# . "$DIR/bash_lib.sh"
-# string='Paris, France, Europe';
-# readarray -td '' a < <(awk '{ gsub(/, /,"\0"); print; }' <<<"$string, "); unset 'a[-1]';
-# declare -p a;
 
-test_dir="$script_dir"
-# test_dir="/opt/ias/bin/blah//src//blah////"
-
-echo "Test dir: $test_dir"
-if is_dir_in_src_dir "$test_dir"
+if is_dir_in_src_dir "$@"
 then
 	echo "Yes."
 else
