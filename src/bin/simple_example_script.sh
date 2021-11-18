995a6be2e6f424438837b28d726b29786ba323fc
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-11-18 16:00:23 -0500

    better with shellcheck now.

--- src/bin/simple_example_script.sh
+++ src/bin/simple_example_script.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-all_arguments=( $@ )
+all_arguments=( "$@" )
 
 . /opt/IAS/lib/bash4/IAS/BashInfra/full_project_lib.sh || exit 1
 
 write_log_start
-write_log_informational "Arguments: ${all_arguments[@]}"
+write_log_informational "Arguments: " "${all_arguments[@]}"
 
-output_file_name=`get_output_file_name`
+output_file_name=$(get_output_file_name)
 
 echo "Here is the configuration file example_config.conf:"
-cat `get_conf_dir`/example_config.conf
+cat "$(get_conf_dir)/example_config.conf"
 
 date >> "$output_file_name"
 
 write_log_debug "Here is a debug message."
 write_log_informational "Wrote: ${output_file_name}"
+exit_status=0
 write_log_informational "Exit status: ${exit_status}"
 write_log_error "This is an example error."
 
