f25dc430c8b5efa53a0767385731976aa2494057
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-11-18 16:09:41 -0500

    better with shellcheck

--- src/bin/bash_infra_test.sh
+++ src/bin/bash_infra_test.sh
@@ -6,21 +6,23 @@ LOG_TO_FILE='1'
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -L )"
 IAS_BASH_INFRA_LIB_DIR="${SCRIPT_PATH}/../lib/bash4/IAS/BashInfra/"
 
-. $IAS_BASH_INFRA_LIB_DIR/full_project_lib.sh || exit 1
+. "$IAS_BASH_INFRA_LIB_DIR/full_project_lib.sh" || exit 1
 
-. ${BASH_FINDBIN_REALBIN}/bash_lib.sh || exit 1
+. "${BASH_FINDBIN_REALBIN}/bash_lib.sh" || exit 1
 
 write_log_start
 
-output_file_name=`get_output_file_name`
+output_file_name=$(get_output_file_name)
 
 # debug_project_variables
 echo "Here is the configuration file example_config.conf:"
-cat "`get_conf_dir`"/example_config.conf
+cat "$(get_conf_dir)"/example_config.conf
 
 write_log_debug "Here is a debug message."
 
 write_log_informational "Wrote: ${output_file_name}"
+
+exit_status=0
 write_log_informational "Exit status: ${exit_status}"
 
 write_log_end
