56ed9ca2fb7a026cc4d7d3bf33eeb977735213e9
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-11-19 10:46:42 -0500

    shellcheck

--- src/lib/bash4/IAS/BashInfra/full_project_lib.sh
+++ src/lib/bash4/IAS/BashInfra/full_project_lib.sh
@@ -2,16 +2,16 @@
 
 IAS_BASH_INFRA_LIB_DIR="$IAS_BASH_INFRA_LIB_DIR"
 if [[ -z "$IAS_BASH_INFRA_LIB_DIR" ]]; then
-	IAS_BASH_INFRA_LIB_DIR=`dirname "${BASH_SOURCE}"`
+	IAS_BASH_INFRA_LIB_DIR=$(dirname "${BASH_SOURCE[0]}")
 fi
 
-. $IAS_BASH_INFRA_LIB_DIR/full_project_variables.sh
-. $IAS_BASH_INFRA_LIB_DIR/FindBin.sh
-. $IAS_BASH_INFRA_LIB_DIR/project_path_chooser.sh
-. $IAS_BASH_INFRA_LIB_DIR/project_standard_functions.sh
-. $IAS_BASH_INFRA_LIB_DIR/full_project_path_functions.sh
-. $IAS_BASH_INFRA_LIB_DIR/full_project_load_environment.sh
-. $IAS_BASH_INFRA_LIB_DIR/bash_log_dispatcher.sh
+. "$IAS_BASH_INFRA_LIB_DIR/full_project_variables.sh"
+. "$IAS_BASH_INFRA_LIB_DIR/FindBin.sh"
+. "$IAS_BASH_INFRA_LIB_DIR/project_path_chooser.sh"
+. "$IAS_BASH_INFRA_LIB_DIR/project_standard_functions.sh"
+. "$IAS_BASH_INFRA_LIB_DIR/full_project_path_functions.sh"
+. "$IAS_BASH_INFRA_LIB_DIR/full_project_load_environment.sh"
+. "$IAS_BASH_INFRA_LIB_DIR/bash_log_dispatcher.sh"
 
 
 check_logging_setup
