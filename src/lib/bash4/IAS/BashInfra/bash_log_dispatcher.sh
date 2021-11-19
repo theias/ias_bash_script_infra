935dc8b5ec27276689457ec3c66c87f0fb3c6428
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-11-19 10:51:44 -0500

    shellcheck

--- src/lib/bash4/IAS/BashInfra/bash_log_dispatcher.sh
+++ src/lib/bash4/IAS/BashInfra/bash_log_dispatcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$LOG_TO_FILE" == "1" ]]; then
-	. ${IAS_BASH_INFRA_LIB_DIR}/filelog_lib.sh
+	. "${IAS_BASH_INFRA_LIB_DIR}/filelog_lib.sh"
 else
-	. ${IAS_BASH_INFRA_LIB_DIR}/syslog_lib.sh
+	. "${IAS_BASH_INFRA_LIB_DIR}/syslog_lib.sh"
 fi
