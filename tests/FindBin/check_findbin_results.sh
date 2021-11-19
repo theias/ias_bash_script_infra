4712e1e32ecd14efce44c90c39e08dbf5a7ecd2e
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-11-19 14:44:53 -0500

    interesting way of doing things.

--- tests/FindBin/check_findbin_results.sh
+++ tests/FindBin/check_findbin_results.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-sed -s 's/\.pl$//' | sed -s 's/\.sh$//'
+script_dir=$(dirname "${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}")
+
+if [[ -z "$0" ]]
+then
+	script_dir=$( dirname $(realpath "$0") )
+else
+	:
+fi
+
+function filter_the_results
+{
+	sed -s 's/\.pl$//' | sed -s 's/\.sh$//'
+}
+
+function check_real_bin
+{
+	diff <( "$script_dir/real_dir/real_bin.pl" | filter_the_results ) \
+		 <( "$script_dir/real_dir/real_bin.sh" | filter_the_results )
+}
+
+function check_sym_bin
+{
+	diff <( "$script_dir/sym_dir/real_bin.pl" | filter_the_results ) \
+		 <( "$script_dir/sym_dir/real_bin.sh" | filter_the_results )
+}
+
