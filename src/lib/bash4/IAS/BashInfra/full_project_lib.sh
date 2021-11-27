6b430d322028ad1ac6f7887de0588840058114ad
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-11-26 21:19:40 -0500

    shellcheck

--- src/lib/bash4/IAS/BashInfra/full_project_lib.sh
+++ src/lib/bash4/IAS/BashInfra/full_project_lib.sh
@@ -5,12 +5,19 @@ if [[ -z "$IAS_BASH_INFRA_LIB_DIR" ]]; then
 	IAS_BASH_INFRA_LIB_DIR=$(dirname "${BASH_SOURCE[0]}")
 fi
 
+# shellcheck disable=SC1090
 . "$IAS_BASH_INFRA_LIB_DIR/full_project_variables.sh"
+# shellcheck disable=SC1090
 . "$IAS_BASH_INFRA_LIB_DIR/FindBin.sh"
+# shellcheck disable=SC1090
 . "$IAS_BASH_INFRA_LIB_DIR/project_path_chooser.sh"
+# shellcheck disable=SC1090
 . "$IAS_BASH_INFRA_LIB_DIR/project_standard_functions.sh"
+# shellcheck disable=SC1090
 . "$IAS_BASH_INFRA_LIB_DIR/full_project_path_functions.sh"
+# shellcheck disable=SC1090
 . "$IAS_BASH_INFRA_LIB_DIR/full_project_load_environment.sh"
+# shellcheck disable=SC1090
 . "$IAS_BASH_INFRA_LIB_DIR/bash_log_dispatcher.sh"
 
 
