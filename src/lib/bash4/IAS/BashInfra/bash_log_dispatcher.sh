36aa8389e1fc83b02c398415a93a06902ff90d26
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-11-26 20:58:33 -0500

    shellcheck ignore

--- src/lib/bash4/IAS/BashInfra/bash_log_dispatcher.sh
+++ src/lib/bash4/IAS/BashInfra/bash_log_dispatcher.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [[ "$LOG_TO_FILE" == "1" ]]; then
+	# shellcheck disable=SC1090
 	. "${IAS_BASH_INFRA_LIB_DIR}/filelog_lib.sh"
 else
+	# shellcheck disable=SC1090
 	. "${IAS_BASH_INFRA_LIB_DIR}/syslog_lib.sh"
 fi
