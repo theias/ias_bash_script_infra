5887682486371bf733dfbcf1e1c79dee25719a16
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-11-26 20:31:14 -0500

    shellcheck ignore

--- src/bin/bash_infra_test.sh
+++ src/bin/bash_infra_test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-
 # NOTE: This runs against the SOURCE TREE version of FindBin...
+# shellcheck disable=SC2034
 LOG_TO_FILE='1'
+# shellcheck disable=SC2164
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -L )"
 IAS_BASH_INFRA_LIB_DIR="${SCRIPT_PATH}/../lib/bash4/IAS/BashInfra/"
-
+# shellcheck disable=SC1090
 . "$IAS_BASH_INFRA_LIB_DIR/full_project_lib.sh" || exit 1
-
+# shellcheck disable=SC1090
 . "${BASH_FINDBIN_REALBIN}/bash_lib.sh" || exit 1
 
 write_log_start
