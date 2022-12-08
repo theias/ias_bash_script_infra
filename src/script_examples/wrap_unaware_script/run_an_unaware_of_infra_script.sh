6480f5236f2e86b0015362e76a9b7f0b0fe4b738
Martin VanWinkle III <mvanwinkle@ias.edu>
2022-12-08 13:20:13 -0500

    progress

--- src/script_examples/wrap_unaware_script/run_an_unaware_of_infra_script.sh
+++ src/script_examples/wrap_unaware_script/run_an_unaware_of_infra_script.sh
@@ -2,6 +2,8 @@
 
 all_arguments=( "$@" )
 
+rest_of_arguments=( "$@" )
+
 # shellcheck disable=SC1091
 . /opt/IAS/lib/bash4/IAS/BashInfra/full_project_lib.sh || exit 1
 
@@ -11,11 +13,20 @@ write_log_start "${all_arguments[@]}"
 
 output_file_name=$(get_output_file_name)
 
-program_base_name=$( basename "$program_to_run" )
-write_log_informational "Running: $program_to_run"
+program_base_name=$( basename "$program_to_run"  )
 
-"$program_to_run" > "$output_file_name" \
-	2> >( sed "s/^/$program_base_name stderr:/" | write_log_informational )
+pbn_escaped=$(printf '%s\n' "$program_base_name" | sed 's/[\/&]/\\&/g')
+write_log_informational "Running: $program_to_run"
+command_to_run=(
+	stdbuf --error=L --output=L "$program_to_run" \
+		"${rest_of_arguments[@]}"
+)
+
+write_log_informational "Full command: ${command_to_run[@]}"
+
+"${command_to_run[@]}" \
+	> "$output_file_name" \
+	2> >( stdbuf --output=L sed "s/^/$pbn_escaped : / " | write_log_informational )
 
 exit_status_of_unaware_script=$?
 
@@ -26,12 +37,10 @@ else
 	write_log_error "$program_to_run" failed to run.  Please check logs.
 fi
 
-exit_status=0 # Whatever we want, based off of how things go
-
 write_log_informational "Wrote: ${output_file_name}"
-write_log_informational "Exit status: ${exit_status}"
+write_log_informational "Exit status: ${exit_status_of_unaware_script}"
 
 write_log_end
 
-exit $exit_status
+exit $exit_status_of_unaware_script
 
